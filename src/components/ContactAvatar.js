af207835a7d736658e832f751a4a139822271541
shubhamtarpara <shubham.t@zuru.tech>
2022-07-06 11:46:27 +0530

    updated avatar

--- src/components/ContactAvatar.js
+++ src/components/ContactAvatar.js
@@ -1,16 +1,17 @@
-import React from "react";
-import md5 from "md5";
-import { Avatar } from "@mui/material";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import md5 from 'md5';
 
 function stringToColor(string) {
-  return "#" + md5(string).slice(0, 6);
+  return '#' + md5(string).slice(0, 6);
 }
+
 function stringAvatar(name) {
   return {
     sx: {
       bgcolor: stringToColor(name),
     },
-    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
   };
 }
 
